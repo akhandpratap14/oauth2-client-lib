e1a62ad672b823c1f08e55420017050c97ea428f
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-12-14 22:07:47 +0530

    removed client id

--- src/app/utils/google-oauth-config.ts
+++ src/app/utils/google-oauth-config.ts
@@ -1,7 +1,6 @@
 export const GOOGLE_OAUTH_CONFIG = {
-  clientId:
-    "54172840654-1kbtddtcnuj10h2gp706kk4hskra4i01.apps.googleusercontent.com",
-  clientSecret: "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",
+  clientId: "**",
+  clientSecret: "**",
   redirectUri: "http://localhost:5173/callback",
   authUrl: "https://accounts.google.com/o/oauth2/v2/auth",
   tokenUrl: "https://oauth2.googleapis.com/token",
