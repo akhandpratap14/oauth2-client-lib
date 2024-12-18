0e52a1680c95bfa15718b2295b520dfd914bb57f
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-12-18 15:35:43 +0530

    removed domain

--- src/app/utils/autho-config.ts
+++ src/app/utils/autho-config.ts
@@ -1,6 +1,6 @@
 export const AUTHO_CONFIG = {
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   redirectUri: "http://localhost:5173/callback",
-  authUrl: "https://dev-adqqhipcwhhafqtf.us.auth0.com/authorize",
-  tokenUrl: "https://dev-adqqhipcwhhafqtf.us.auth0.com/oauth/token",
+  authUrl: "https://DOMAIN/authorize",
+  tokenUrl: "https://DOMAIN/oauth/token",
 };
