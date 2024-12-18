22428e7f7972450dfa2217a084853bbd92a90d62
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-12-18 15:46:40 +0530

    removed redirect url to env

--- src/app/utils/autho-config.ts
+++ src/app/utils/autho-config.ts
@@ -1,6 +1,6 @@
 export const AUTHO_CONFIG = {
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  redirectUri: "http://localhost:5173/callback",
-  authUrl: "https://DOMAIN/authorize",
-  tokenUrl: "https://DOMAIN/oauth/token",
+  redirectUri: import.meta.env.VITE_APP_REDIRECTURL,
+  authUrl: "https://dev-adqqhipcwhhafqtf.us.auth0.com/authorize",
+  tokenUrl: "https://dev-adqqhipcwhhafqtf.us.auth0.com/oauth/token",
 };
